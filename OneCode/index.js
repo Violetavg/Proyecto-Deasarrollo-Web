2cfb4d360c98762acd1cb18ab8f22606729cd00b
Violetavg <almavioleta96@gmail.com>
2018-12-12 04:01:36 -0600

    cambios varios
    agregadas dependencias para mysql, pasport, entre otras

--- OneCode/index.js
+++ OneCode/index.js
@@ -1,5 +1,18 @@
-var express = require("express"); //Importar el framework express
-        var app = express(); //Inicializar la aplicacion con express
-        app.use(express.static("public")); //Publicar un directorio de archivos estaticos
-        app.listen(8001);//Levantar el servidor y escuchar en el puerto indicado
+var express = require("express");
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
+var app = express();
+var credenciales = {
+        host:"localhost",
+        user:"root",
+        password:"",
+        port:"3306",
+        database: "one_code"
+};
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(8111, function(){ console.log("Servidor Iniciado");});
         
